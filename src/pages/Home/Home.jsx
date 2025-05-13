762d32e653182f17ae98dc6ddbaf76ef0380f34d
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-05-13 20:21:08 +0100

    Hero Section Design

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import './Home.css'
+import jobSearchGlobe from '../../assets/jobSearchGlobe.png'
+import artisan from '../../assets/artisan.avif'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+        <div className="homeTxt">
+            <p>Hire skilled workers you can rely on, Grow your service and find jobs easily - All in One</p>
+
+            <div className="ctaBtns">
+                <button className='exploreServices'>Explore Services</button>
+                <button className='becomeProvider'>Become a Provider</button>
+            </div>
+        </div>
+        <img src={artisan} alt="" className='homeBanner'/>
+        <img src={jobSearchGlobe} alt=""  className='jobSearch'/>
+    </div>
   )
 }
 
