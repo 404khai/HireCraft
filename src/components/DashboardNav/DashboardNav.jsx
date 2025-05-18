988c3623a38ff1e9efbebe08e316e6f752f88a79
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-05-18 20:40:20 +0100

    Service Provider SideNav

---
+++ src/components/DashboardNav/DashboardNav.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './DashboardNav.css'
+
+const DashboardNav = () => {
+  return (
+    <div>DashboardNav</div>
+  )
+}
+
+export default DashboardNav
--- src/components/Sidenav/Sidenav.jsx
+++ src/components/Sidenav/Sidenav.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Sidenav.css'
 import { Link } from 'react-router-dom'
-import { HiWrenchScrewdriver } from "react-icons/hi2";
+import { HiWrenchScrewdriver, HiOutlineWallet } from "react-icons/hi2";
 import { LuLayoutDashboard, LuBellRing } from "react-icons/lu";
 import { TiMessages } from "react-icons/ti";
 import { FaRegUser } from "react-icons/fa6";
-import { IoExitOutline } from "react-icons/io5";
+import { IoExitOutline , IoBriefcaseOutline} from "react-icons/io5";
+import { MdOutlineRateReview } from "react-icons/md";
 
 const Sidenav = () => {
   return (
@@ -18,7 +19,7 @@ const Sidenav = () => {
         </Link>
 
         <div className="start">
-            <p>Start</p>
+            <p><b>Start</b></p>
             <Link to="/Dashboard">
                 <i><LuLayoutDashboard /></i>
                 Dashboard
@@ -31,13 +32,25 @@ const Sidenav = () => {
                 <i><LuBellRing /></i>
                 Job Requests
             </Link>
+            <Link to="/Dashboard">
+                <i><IoBriefcaseOutline /></i>
+                My Bookings
+            </Link>
         </div>
-
+       
         <div className="account">
-            <p>Account</p>
+            <p><b>Account</b></p>
             <Link to="/Dashboard">
                 <i><FaRegUser /></i>
                 My Profile
+            </Link>
+            <Link to="/Dashboard">
+                <i><HiOutlineWallet /></i>
+                Wallet / Earnings
+            </Link>
+            <Link to="/Dashboard">
+                <i><MdOutlineRateReview /></i>
+                Review & Ratings
             </Link>
             <Link to="/Dashboard">
                 <i><IoExitOutline /></i>
