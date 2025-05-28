46ba15143a895254fb0e1fe835a4eefaedebc9aa
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-05-28 23:36:55 +0100

    Dropdown on DashboardNav

--- src/components/DashboardNav/DashboardNav.jsx
+++ src/components/DashboardNav/DashboardNav.jsx
@@ -1,31 +1,90 @@
+// import './DashboardNav.css'
+// import profilePic from '../../assets/pacman.jpg'
+// // import profilePic from '../../assets/bluePacman.png'
+// import { IoHomeOutline } from "react-icons/io5";
+// import { PiMailbox } from "react-icons/pi";
+// import { Link } from 'react-router-dom';
+// import React from 'react';
+// // import Toggle from '../../components/Toggle/Toggle'
+
+// const DashboardNav = () => {
+//   return (
+//     <div className='dashboardNav'>
+//         <Link to="/Home" className="dashboardLogoLink">
+//             <div className="dashboardLogo">
+//                 <p>hire<span className="navLogoSpan">craft</span></p>
+//             </div>
+//         </Link>
+
+//         <div className='dashboardNavControls'>
+//           <Link to="/Home">
+//               <i className='notifications'><IoHomeOutline /></i>
+//           </Link>
+//           {/* <div className="moon">
+//               <Toggle/>
+//           </div> */}
+//           <i className='notifications'><PiMailbox /></i>
+//           <img src={profilePic} alt="" className='profilePic'/>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default DashboardNav
+
 import './DashboardNav.css'
 import profilePic from '../../assets/pacman.jpg'
-// import profilePic from '../../assets/bluePacman.png'
-import { IoHomeOutline } from "react-icons/io5";
-import { PiMailbox } from "react-icons/pi";
-import { Link } from 'react-router-dom';
-import React from 'react';
-// import Toggle from '../../components/Toggle/Toggle'
+import { IoExitOutline, IoHomeOutline } from "react-icons/io5"
+import { PiMailbox } from "react-icons/pi"
+import { Link } from 'react-router-dom'
+import React, { useState, useRef, useEffect } from 'react'
+import { FaRegUser } from 'react-icons/fa6'
 
 const DashboardNav = () => {
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false)
+  const dropdownRef = useRef(null)
+
+  const toggleDropdown = () => {
+    setIsDropdownVisible(prev => !prev)
+  }
+
+  // Close dropdown on outside click
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsDropdownVisible(false)
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside)
+    return () => document.removeEventListener("mousedown", handleClickOutside)
+  }, [])
+
   return (
     <div className='dashboardNav'>
-        <Link to="/Home" className="dashboardLogoLink">
-            <div className="dashboardLogo">
-                <p>hire<span className="navLogoSpan">craft</span></p>
-            </div>
-        </Link>
-
-        <div className='dashboardNavControls'>
-          <Link to="/Home">
-              <i className='notifications'><IoHomeOutline /></i>
-          </Link>
-          {/* <div className="moon">
-              <Toggle/>
-          </div> */}
-          <i className='notifications'><PiMailbox /></i>
-          <img src={profilePic} alt="" className='profilePic'/>
+      <Link to="/Home" className="dashboardLogoLink">
+        <div className="dashboardLogo">
+          <p>hire<span className="navLogoSpan">craft</span></p>
         </div>
+      </Link>
+
+      <div className='dashboardNavControls'>
+        <Link to="/Home">
+          <i className='notifications'><IoHomeOutline /></i>
+        </Link>
+        <i className='notifications'><PiMailbox /></i>
+
+        <div className='profileWrapper' onClick={toggleDropdown}>
+          <img src={profilePic} alt="Profile" className='profilePic' />
+          {isDropdownVisible && (
+            <div className="profileDropdown" ref={dropdownRef}>
+              <Link to="/ProviderProfile"><i><FaRegUser /></i>My Profile</Link>
+              <Link to="/Home"><i><IoHomeOutline /></i>Back to Home</Link>
+              <Link to=""><i><IoExitOutline /></i>Log Out</Link>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/pages/ProviderProfile/ProviderProfile.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ProviderProfile.css'
+
+const ProviderProfile = () => {
+  return (
+    <div>ProviderProfile</div>
+  )
+}
+
+export default ProviderProfile
