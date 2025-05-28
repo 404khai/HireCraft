308aad41df164b1baed58b52d528b28d19bde109
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-05-28 01:18:19 +0100

    Edit Profile Form

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Settings.css'
 import DashboardNav from '../../components/DashboardNav/DashboardNav'
 import ProviderSideNav from '../../components/ProviderSideNav/ProviderSideNav'
 import Breadcrumbs from '../../components/Breadcrumbs/Breadcrumbs'
 import { TiFolderAdd } from "react-icons/ti";
+import avatar from '../../assets/pacman.jpg'
+
+
+import { CountrySelect, StateSelect, CitySelect } from 'react-country-state-city'
+import "react-country-state-city/dist/react-country-state-city.css";
+import PhoneInput from 'react-phone-number-input';
+import 'react-phone-number-input/style.css';
 
 const Settings = () => {
+  const [countryid, setcountryid] = useState(0);
+  const [stateid, setstateid] = useState(0);
+  
+  const [PhoneNumber, setPhoneNumber] = useState('');
+  
+  const handleChange =(value) =>{
+    setPhoneNumber(value);
+  };
+
   return (
     <div className='profileBox'>
       <DashboardNav/>
@@ -23,10 +39,97 @@ const Settings = () => {
 
                 <div className="editProfileSettings">
                     <div className="editProfile">
-                        <div className="jobAlertsHead">
+                        <div className="editProfileHead">
                             <i><TiFolderAdd /></i>
                             <b>Edit Profile</b>
                         </div>
+
+                        <div className="editPic">
+                          <p><b>Avatar</b></p>
+                          <img src={avatar} alt="" title='Change Avatar'/>
+                        </div>
+
+                        <div className="changeInfoForm">
+                          <div className="changeInfoInputContainer">
+                            <label className="changeInfoLabel">Firstname</label>
+                            <input placeholder="Tom" title="Change Firstname" type="text" className="changeInfoInput" required/>
+                          </div>
+
+                          <div className="changeInfoInputContainer">
+                            <label className="changeInfoLabel">Lastname</label>
+                            <input placeholder="Holland" title="Change Lastname" type="text" className="changeInfoInput" required/>
+                          </div>
+
+                          <div className="changeInfoInputContainer">
+                            <label className="changeInfoLabel">Email</label>
+                            <input placeholder="testhirecraft@gmail.com" title="Change Lastname" type="text" className="changeInfoInput" required/>
+                          </div>
+
+                          <div className="group" id='phoneNo'>
+                            <label htmlFor="" >
+                                <PhoneInput
+                                    international
+                                    countryCallingCodeEditable={false}
+                                    defaultCountry="NG" // Use the correct country code (e.g., "GB" for the UK)
+                                    value={PhoneNumber}
+                                    onChange={handleChange}  
+                                />     
+                            </label>     
+                          </div>
+
+                          <div className="changeInfoInputContainer">
+                            <label className="changeInfoLabel">Profession</label>
+                            <input placeholder="Animator" title="Change Lastname" type="text" className="changeInfoInput" required/>
+                          </div>
+
+                          <div className="changeInfoInputContainer">
+                            <label className="changeInfoLabel">Bio</label>
+                            <input placeholder="" title="Change Lastname" type="text" className="changeInfoInput" required/>
+                          </div>
+                        </div>
+                        
+                        <div className="input_container" id='country'>
+                          <label className="input_label" for="password_field">Select Country</label>
+                          <CountrySelect 
+                              onChange={(e) =>{
+                                  setcountryid(e.id);
+                                  console.log(e);  
+                              }}
+                              style={{border: "none", outline: "none"}}
+                              // placeHolder='Select Country'
+                              className="input_field"
+                          />
+                        </div>
+
+                    <div className="input_container" id='state'>
+                        <label className="input_label" for="password_field">Select State</label>
+                        <StateSelect 
+                            disabled={!countryid}
+                            countryid={countryid}
+                            onChange={(e) =>{
+                                setstateid(e.id);
+                                console.log(e);  
+                            }}
+                            style={{border: "none", outline: "none"}}
+                            // placeHolder='Select State'
+                            className='input_field'
+                        />
+                    </div>
+
+                    <div className="input_container" id='state'>
+                        <label className="input_label" for="password_field">Select City</label>
+                        <CitySelect 
+                            disabled={!stateid}
+                            countryid={countryid}
+                            stateid={stateid}
+                            onChange={(e) =>{
+                                console.log(e);  
+                            }}
+                            style={{border: "none", outline: "none"}}
+                            // placeHolder='Select City'
+                            className='input_field'
+                        />
+                    </div>
                     </div>
                 </div>
 
