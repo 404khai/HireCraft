379cf5a8284e09d16bdf3e56578d8cf3e6632dc2
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-05-29 11:39:38 +0100

    ContactUs Page

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -3,7 +3,19 @@ import './ContactUs.css'
 
 const ContactUs = () => {
   return (
-    <div>ContactUs</div>
+    <div className='contactUsPage'>
+      <div className="contactUsPageTxt">
+        <h3>Get in touch</h3>
+        <h1>WE'RE HERE TO HELP, 24/7</h1>
+      </div>
+
+      <div className="contactUsPageBox">
+          <h3>Contact Us</h3>
+          <p>Reach out to us via email or chat for immediate assistance.</p>
+          <p>support@hirecraft.com</p>
+          <p>+2347041049407</p>
+      </div>
+    </div>
   )
 }
 
