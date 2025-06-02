8479d78ec2e9fe918818473d58fef9ba52cd28a8
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-06-02 17:44:04 +0100

    Freelancer Profile Component

--- src/components/FreelancerProfile/FreelancerProfile.jsx
+++ src/components/FreelancerProfile/FreelancerProfile.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
 import './FreelancerProfile.css'
+import { Link } from 'react-router-dom';
+import OIF from '../../assets/OIF.jpeg'
+import { IoLocationOutline } from "react-icons/io5";
+import { FaStar, FaRegStarHalfStroke } from "react-icons/fa6";
 
 const FreelancerProfile = () => {
   return (
-    <div>FreelancerProfile</div>
+    <div className='freelancerProfile'>
+        <img src={OIF} alt="" />
+        <div className="freelancerProfileInfo">
+            <p className='freelanceProfileInfoName'><b>Lucas Harris</b></p>
+            <p className='freelanceProfileInfoJob'>UI/UX Design</p>
+            <div className="providerRating">
+                <button>4.5</button>
+                <i><FaStar/></i>
+                <i><FaStar/></i>
+                <i><FaStar/></i>
+                <i><FaStar/></i>
+                <i><FaRegStarHalfStroke/></i>
+            </div>
+        </div>
+        <div className="freelancerProfileInfo2">
+            <div className='freelancerProfileRateBox'>
+                <div className='freelancerProfileRate'>
+                    <p>Rate</p>
+                    <strong>$90 / hour</strong>
+                </div>
+                <div className='freelancerProfileLocation'>
+                    <p>Location</p>
+                    <strong> <i><IoLocationOutline /></i>Chicago, Illinois</strong>
+                </div>
+            </div>
+
+            <button className='viewProfileBtn'>
+                <Link to="/ProviderProfile">View Profile</Link>
+            </button>
+        </div>
+    </div>
   )
 }
 
