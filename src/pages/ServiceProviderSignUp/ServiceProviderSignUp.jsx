05108ebdf0dc6123dba721a9a25465ec18d5a49d
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-06-14 13:50:52 +0100

    Register auth debugging

--- src/pages/ServiceProviderSignUp/ServiceProviderSignUp.jsx
+++ src/pages/ServiceProviderSignUp/ServiceProviderSignUp.jsx
@@ -9,22 +9,9 @@ import PhoneInput from 'react-phone-number-input';
 import 'react-phone-number-input/style.css';
 
 const ServiceProviderSignUp = () => {
-
-    // const [step, setStep] = useState(0);
-    // const [countryid, setcountryid] = useState(0);
-    // const [stateid, setstateid] = useState(0);
-    // const [email, setEmail] = useState('');
-    // const [password, setPassword] = useState('');
-    // const [firstName, setFirstName] = useState('');
-    // const [PhoneNumber, setPhoneNumber] = useState('');
-
-    // const handleChange =(value) =>{
-    //     setPhoneNumber(value);
-    // };
     const location = useLocation(); // Get the location object
     const [step, setStep] = useState(0);
-    const [countryid, setcountryid] = useState(0);
-    const [stateid, setstateid] = useState(0);
+   
     const [formData, setFormData] = useState({
         firstname: '',
         lastname: '',
@@ -33,10 +20,12 @@ const ServiceProviderSignUp = () => {
         phoneNumber: '',
         occupation: '', // Add occupation to formData
         hourlyRate: '', // Add hourlyRate to formData
-        country: '', // Add country
-        state: '',   // Add state
+        country: '',
+        countryId: 0,
+        state: '',
+        stateId: 0,
         city: '',    // Add city
-        role: location.state?.role || "SERVICE_PROVIDER" // Initialize role from state or default
+        role: location.state?.role || "ROLE_PROVIDER" // Initialize role from state or default
     });
 
 
@@ -111,7 +100,7 @@ const ServiceProviderSignUp = () => {
             {step === 0 && (
                 <div className='firstStep'>
                     <div className="input_container">
-                        <label className="input_label" for="email_field">Firstname</label>
+                        <label className="input_label" htmlFor="email_field">Firstname</label>
                         <svg xmlns="http://www.w3.org/2000/svg" className="icon" fill="none" viewBox="0 0 24 24" stroke="#141B34" strokeWidth="1.5">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4z" />
                             <path strokeLinecap="round" strokeLinejoin="round" d="M4 20c0-3.31 3.58-6 8-6s8 2.69 8 6" />
@@ -130,7 +119,7 @@ const ServiceProviderSignUp = () => {
                     </div>
 
                     <div className="input_container">
-                        <label className="input_label" for="email_field">Lastname</label>
+                        <label className="input_label" htmlFor="email_field">Lastname</label>
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             className="icon"
@@ -183,7 +172,7 @@ const ServiceProviderSignUp = () => {
             {step === 1 && (
                 <div className="secondStep">
                     <div className="input_container">
-                        <label className="input_label" for="email_field">Email</label>
+                        <label className="input_label" htmlFor="email_field">Email</label>
                         <svg fill="none" viewBox="0 0 24 24" height="24" width="24" xmlns="http://www.w3.org/2000/svg" className="icon">
                             <path stroke-linejoin="round" stroke-linecap="round" stroke-width="1.5" stroke="#141B34" d="M7 8.5L9.94202 10.2394C11.6572 11.2535 12.3428 11.2535 14.058 10.2394L17 8.5"></path>
                             <path stroke-linejoin="round" stroke-width="1.5" stroke="#141B34" d="M2.01577 13.4756C2.08114 16.5412 2.11383 18.0739 3.24496 19.2094C4.37608 20.3448 5.95033 20.3843 9.09883 20.4634C11.0393 20.5122 12.9607 20.5122 14.9012 20.4634C18.0497 20.3843 19.6239 20.3448 20.7551 19.2094C21.8862 18.0739 21.9189 16.5412 21.9842 13.4756C22.0053 12.4899 22.0053 11.5101 21.9842 10.5244C21.9189 7.45886 21.8862 5.92609 20.7551 4.79066C19.6239 3.65523 18.0497 3.61568 14.9012 3.53657C12.9607 3.48781 11.0393 3.48781 9.09882 3.53656C5.95033 3.61566 4.37608 3.65521 3.24495 4.79065C2.11382 5.92608 2.08114 7.45885 2.01576 10.5244C1.99474 11.5101 1.99475 12.4899 2.01577 13.4756Z"></path>
@@ -202,7 +191,7 @@ const ServiceProviderSignUp = () => {
                     </div>
 
                     <div className="input_container">
-                        <label className="input_label" for="password_field">Password</label>
+                        <label className="input_label" htmlFor="password_field">Password</label>
                         <svg fill="none" viewBox="0 0 24 24" height="24" width="24" xmlns="http://www.w3.org/2000/svg" className="icon">
                             <path stroke-linecap="round" stroke-width="1.5" stroke="#141B34" d="M18 11.0041C17.4166 9.91704 16.273 9.15775 14.9519 9.0993C13.477 9.03404 11.9788 9 10.329 9C8.67911 9 7.18091 9.03404 5.70604 9.0993C3.95328 9.17685 2.51295 10.4881 2.27882 12.1618C2.12602 13.2541 2 14.3734 2 15.5134C2 16.6534 2.12602 17.7727 2.27882 18.865C2.51295 20.5387 3.95328 21.8499 5.70604 21.9275C6.42013 21.9591 7.26041 21.9834 8 22"></path>
                             <path stroke-linejoin="round" stroke-linecap="round" stroke-width="1.5" stroke="#141B34" d="M6 9V6.5C6 4.01472 8.01472 2 10.5 2C12.9853 2 15 4.01472 15 6.5V9"></path>
@@ -228,7 +217,7 @@ const ServiceProviderSignUp = () => {
                 <div className="fourthStep">
                     <div className="group" id='phoneNo'>
                         <label htmlFor="" >
-                            <label className="input_label" for="email_field">Phone Number</label>
+                            <label className="input_label" htmlFor="email_field">Phone Number</label>
                             <PhoneInput
                                 international
                                 countryCallingCodeEditable={false}
@@ -242,7 +231,7 @@ const ServiceProviderSignUp = () => {
                     </div>
 
                     <div className="input_container">
-                        <label className="input_label" for="email_field">Occupation</label>
+                        <label className="input_label" htmlFor="email_field">Occupation</label>
                         <svg fill="none" viewBox="0 0 24 24" height="24" width="24" xmlns="http://www.w3.org/2000/svg" className="icon">
                             <path stroke-linejoin="round" stroke-linecap="round" stroke-width="1.5" stroke="#141B34" d="M7 8.5L9.94202 10.2394C11.6572 11.2535 12.3428 11.2535 14.058 10.2394L17 8.5"></path>
                             <path stroke-linejoin="round" stroke-width="1.5" stroke="#141B34" d="M2.01577 13.4756C2.08114 16.5412 2.11383 18.0739 3.24496 19.2094C4.37608 20.3448 5.95033 20.3843 9.09883 20.4634C11.0393 20.5122 12.9607 20.5122 14.9012 20.4634C18.0497 20.3843 19.6239 20.3448 20.7551 19.2094C21.8862 18.0739 21.9189 16.5412 21.9842 13.4756C22.0053 12.4899 22.0053 11.5101 21.9842 10.5244C21.9189 7.45886 21.8862 5.92609 20.7551 4.79066C19.6239 3.65523 18.0497 3.61568 14.9012 3.53657C12.9607 3.48781 11.0393 3.48781 9.09882 3.53656C5.95033 3.61566 4.37608 3.65521 3.24495 4.79065C2.11382 5.92608 2.08114 7.45885 2.01576 10.5244C1.99474 11.5101 1.99475 12.4899 2.01577 13.4756Z"></path>
@@ -265,42 +254,46 @@ const ServiceProviderSignUp = () => {
             {step === 3 && (
                 <div className='thirdStep'>
                     <div className="input_container" id='country'>
-                        <label className="input_label" for="password_field">Select Country</label>
+                        <label className="input_label" htmlFor="password_field">Select Country</label>
                         <CountrySelect
+                            key={formData.countryId || 'initial_country'}
                             onChange={(e) => {
-                                setcountryid(e.id);
-                                setFormData({ ...formData, country: e.name }); // Store country name
+                                setFormData(prevData => ({ ...prevData, country: e.name, countryId: e.id, state: '', stateId: 0, city: '' }));
                             }}
                             style={{border: "none", outline: "none"}}
                             className="input_field"
+                            value={formData.countryId}
                         />
                     </div>
 
                     <div className="input_container" id='state'>
-                        <label className="input_label" for="password_field">Select State</label>
+                        <label className="input_label" htmlFor="password_field">Select State</label>
                         <StateSelect
-                            disabled={!countryid}
-                            countryid={countryid}
+                            key={formData.stateId || 'initial_state'}
+                            disabled={!formData.countryId} // Use formData.countryId
+                            countryid={formData.countryId} // Use formData.countryId
                             onChange={(e) => {
-                                setstateid(e.id);
-                                setFormData({ ...formData, state: e.name }); // Store state name
+                                setFormData(prevData => ({ ...prevData, state: e.name, stateId: e.id, city: '' })); // Update state name and ID, reset city
                             }}
                             style={{border: "none", outline: "none"}}
                             className='input_field'
+                            value={formData.stateId}
                         />
                     </div>
 
                     <div className="input_container" id='state'>
-                        <label className="input_label" for="password_field">Select City</label>
+                        <label className="input_label" htmlFor="password_field">Select City</label>
                         <CitySelect
-                            disabled={!stateid}
-                            countryid={countryid}
-                            stateid={stateid}
+                            key={formData.city || 'initial_city'}
+                            disabled={!formData.stateId} // Use formData.stateId
+                            countryid={formData.countryId} // Use formData.countryId
+                            stateid={formData.stateId} // Use formData.stateId
                             onChange={(e) => {
-                                setFormData({ ...formData, city: e.name }); // Store city name
+                                setFormData(prevData => ({ ...prevData, city: e.name })); // Update city name
                             }}
                             style={{border: "none", outline: "none"}}
                             className='input_field'
+                            value={formData.city}
                         />
                     </div>
                 
@@ -310,7 +303,7 @@ const ServiceProviderSignUp = () => {
             {step === 4 && (
                 <div className="fifthStep">
                     <div className="input_container">
-                        <label className="input_label" for="email_field">Hourly Rate</label>
+                        <label className="input_label" htmlFor="email_field">Hourly Rate</label>
                         <svg fill="none" viewBox="0 0 24 24" height="24" width="24" xmlns="http://www.w3.org/2000/svg" className="icon">
                             <path stroke-linejoin="round" stroke-linecap="round" stroke-width="1.5" stroke="#141B34" d="M7 8.5L9.94202 10.2394C11.6572 11.2535 12.3428 11.2535 14.058 10.2394L17 8.5"></path>
                             <path stroke-linejoin="round" stroke-width="1.5" stroke="#141B34" d="M2.01577 13.4756C2.08114 16.5412 2.11383 18.0739 3.24496 19.2094C4.37608 20.3448 5.95033 20.3843 9.09883 20.4634C11.0393 20.5122 12.9607 20.5122 14.9012 20.4634C18.0497 20.3843 19.6239 20.3448 20.7551 19.2094C21.8862 18.0739 21.9189 16.5412 21.9842 13.4756C22.0053 12.4899 22.0053 11.5101 21.9842 10.5244C21.9189 7.45886 21.8862 5.92609 20.7551 4.79066C19.6239 3.65523 18.0497 3.61568 14.9012 3.53657C12.9607 3.48781 11.0393 3.48781 9.09882 3.53656C5.95033 3.61566 4.37608 3.65521 3.24495 4.79065C2.11382 5.92608 2.08114 7.45885 2.01576 10.5244C1.99474 11.5101 1.99475 12.4899 2.01577 13.4756Z"></path>
