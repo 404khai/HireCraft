09aecf327f02a0a5ceb9374006e29bfc338f19dc
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-07-05 08:40:59 +0100

    Home page ui tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { AuthProvider  } from './context/AuthContext'
 import { ToastContainer } from 'react-toastify'; // Import ToastContainer
 import 'react-toastify/dist/ReactToastify.css';
 
+
 function App() {
 
   return (
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Home.css'
 import { IoSearch } from "react-icons/io5";
 import { MdHomeRepairService } from "react-icons/md";
@@ -22,7 +22,7 @@ import "slick-carousel/slick/slick-theme.css";
 import "react-multi-carousel/lib/styles.css";
 
 import ServicesScroll from '../../components/ServicesScroll/ServicesScroll';
-
+import workerIcon from '../../assets/icons-worker.png'
 import popularServices from '../../json/popularServices.json'
 import Footer from '../../components/Footer/Footer';
 
@@ -59,6 +59,38 @@ const Home = () => {
         }
     ]
 
+    const [activeAccordion, setActiveAccordion] = useState(null);
+
+  const toggleAccordion = (index) => {
+    setActiveAccordion(activeAccordion === index ? null : index);
+  };
+
+  const faqData = [
+    {
+      id: 'Questions-collapseOne',
+      headingId: 'Questions-headingOne',
+      question: 'Lorem ipsum dolor sit amet adipisicing ?',
+      answer: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime quos voluptatum at, quibusdam blanditiis saepe soluta laborum, repellendus nemo id porro dolor eveniet perspiciatis veritatis doloremque aliquam nam! Libero, nostrum!'
+    },
+    {
+      id: 'Questions-collapseTwo',
+      headingId: 'Questions-headingTwo',
+      question: 'Lorem ipsum dolor sit amet adipisicing ?',
+      answer: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime quos voluptatum at, quibusdam blanditiis saepe soluta laborum, repellendus nemo id porro dolor eveniet perspiciatis veritatis doloremque aliquam nam! Libero, nostrum!'
+    },
+    {
+      id: 'Questions-collapseThree',
+      headingId: 'Questions-headingThree',
+      question: 'Lorem ipsum dolor sit amet adipisicing ?',
+      answer: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime quos voluptatum at, quibusdam blanditiis saepe soluta laborum, repellendus nemo id porro dolor eveniet perspiciatis veritatis doloremque aliquam nam! Libero, nostrum!'
+    },
+    {
+      id: 'Questions-collapseFour',
+      headingId: 'Questions-headingFour',
+      question: 'Lorem ipsum dolor sit amet adipisicing ?',
+      answer: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime quos voluptatum at, quibusdam blanditiis saepe soluta laborum, repellendus nemo id porro dolor eveniet perspiciatis veritatis doloremque aliquam nam! Libero, nostrum!'
+    }
+  ];
 
   return (
     <div className='home'>
@@ -123,12 +155,65 @@ const Home = () => {
                             <h1>7+</h1>
                             <p><b>Service Providers</b></p>
                         </div>
-                        <img decoding="async" width="49" height="45" src="https://studyhub.themewant.com/wp-content/uploads/2024/03/icon.svg" alt="" />
+                        <img src={workerIcon} alt="" className='topImgimg'/>
                     </div>
                 </div>
                 
             </div>
         </div>
+
+        {/* <section className="py-5">
+            <div className="faqcontainer">
+                <div className="row justify-content-center text-center mb-3">
+                <div className="col-lg-8 col-xl-7">
+                    <span className="text-muted">F.A.Q</span>
+                    <h2 className="display-5 fw-bold">Frequently Asked Questions</h2>
+                    <p className="lead">Lorem ipsum dolor sit, amet consectetur adipisicing elit Consequatur quidem eius cum voluptatum quasi delectus.</p>
+                </div>
+                </div>
+                <div className="row justify-content-center">
+                <div className="col-md-5">
+                    <span className="text-muted">Lorem ipsum dolor</span>
+                    <h2 className="pb-4 fw-bold">Have Any Questions?</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur quidem eius cum voluptatum quasi delectus assumenda culpa.</p>
+                    <a className="btn btn-primary btn-lg mt-3" href="#">Contact us</a>
+                </div>
+                <div className="col-md-7">
+                    <div className="accordion" id="Questions-accordion">
+                    {faqData.map((item, index) => (
+                        <div className="accordion-item" key={index}>
+                        <h2 className="accordion-header" id={item.headingId}>
+                            <button
+                            aria-controls={item.id}
+                            aria-expanded={activeAccordion === index}
+                            className={`accordion-button ${activeAccordion === index ? '' : 'collapsed'} bg-light`}
+                            onClick={() => toggleAccordion(index)}
+                            type="button"
+                            >
+                            <div className="text-muted me-3">
+                                <svg className="bi bi-question-circle-fill" fill="currentColor" height="24" viewBox="0 0 16 16" width="24" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.496 6.033h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286a.237.237 0 0 0 .241.247zm2.325 6.443c.61 0 1.029-.394 1.029-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94 0 .533.425.927 1.01.927z"></path>
+                                </svg>
+                            </div>
+                            {item.question}
+                            </button>
+                        </h2>
+                        <div
+                            aria-labelledby={item.headingId}
+                            className={`accordion-collapse collapse ${activeAccordion === index ? 'show' : ''}`}
+                            id={item.id}
+                        >
+                            <div className="accordion-body">
+                            {item.answer}
+                            </div>
+                        </div>
+                        </div>
+                    ))}
+                    </div>
+                </div>
+                </div>
+            </div>
+        </section> */}
 
         <Footer/>
         {/* <div className="howItWorks">
